d76f92147c0be565097f9df9f9dbd0140e6ccbc9
Jsyr963 <89968301+Jsyr963@users.noreply.github.com>
2021-09-15 10:18:55 +0200

    Eerste commit!

---
+++ speelhaldagvaninputnaaroutput.py
@@ -0,0 +1,6 @@
+aantaltickets = int(input("hoeveel tickets wilt u hebben ? : "))
+prijstickets = float(input("Weet u hoeveel 1 ticket kost ? : "))
+
+prijsper5min = float(input("weet u hoeveel 5 miuten kost ? : "))
+aantalkeer = int(input("hoeveel keer wilt u dit spelletje spelen ? : "))
+aantalpersonen = int(input("met hoeveel personen ga je dit spelletje spelen ? : "))
